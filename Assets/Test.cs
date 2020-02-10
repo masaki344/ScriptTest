00a8d641a261238b17c93b581d5a94e3285ace61
masaki344 <masakimiyoshi18.v.v@icloud.com>
2020-02-10 16:26:02 +0900

    ScriptTestプロジェクト　スクリプトの作成
    課題の再提出

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,41 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
 
     void Start ()
     {
-        for(int zyun = 0; zyun <= 1; zyun++)
+        int[] array = { 2, 4, 3, 1, 5 };
+
+        for (int i = 0; i < array.Length; i++)
         {
-            int[] array = { 2, 4, 3, 1, 5 };
-            int[] hako = new int[5];
-            for (int a = 0; a < array.Length; a++)
-            {
-                int n = 0;
-                for (int b = 0; b < array.Length; b++)
-                {
-                    if (zyun == 0)
-                    {
-                        if (array[a] > array[b])
-                        {
-                            n++;
-                        }
-                    }
-                    else if (zyun == 1)
-                    {
-                        if (array[a] < array[b])
-                        {
-                            n++;
-                        }
-                    }
-                }
-                hako[n] = array[a];
-            }
-            for (int c = 0; c < array.Length; c++)
-            {
-                Debug.Log(hako[c]);
-            }
-        } 
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
     }
 	
 	void Update ()
