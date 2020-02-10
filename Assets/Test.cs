ec2eb3d99526a7338058a9a1180ea83ce554ab87
masaki344 <masakimiyoshi18.v.v@icloud.com>
2020-02-10 12:21:30 +0900

    ScriptTestプロジェクト　スクリプト作成
    課題、発展課題の提出

---
+++ Assets/Test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    void Start ()
+    {
+        for(int zyun = 0; zyun <= 1; zyun++)
+        {
+            int[] array = { 2, 4, 3, 1, 5 };
+            int[] hako = new int[5];
+            for (int a = 0; a < array.Length; a++)
+            {
+                int n = 0;
+                for (int b = 0; b < array.Length; b++)
+                {
+                    if (zyun == 0)
+                    {
+                        if (array[a] > array[b])
+                        {
+                            n++;
+                        }
+                    }
+                    else if (zyun == 1)
+                    {
+                        if (array[a] < array[b])
+                        {
+                            n++;
+                        }
+                    }
+                }
+                hako[n] = array[a];
+            }
+            for (int c = 0; c < array.Length; c++)
+            {
+                Debug.Log(hako[c]);
+            }
+        } 
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Test1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+
+    public class Boss
+    {
+        private int hp = 100;
+        private int power = 25;
+        private int mp = 53;
+
+        public void Attack()
+        {
+            Debug.Log(this.power + "のダメージを与えた");
+        }
+
+        public void Defence(int damage)
+        {
+            Debug.Log(damage + "のダメージを受けた");
+            this.hp -= damage;
+        }
+
+        public void Magic()
+        {
+            if (mp >= 5)
+            {
+                this.mp -= 5;
+                Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+            }
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない。");
+            }
+        }
+    }
+
+        void Start ()
+    {
+        Boss lastboss = new Boss();
+
+        for (int i = 1; i <= 11; i++)
+        {
+            lastboss.Magic();
+        }
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
