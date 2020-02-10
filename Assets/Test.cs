b795c840a8faaf12313f8f9fe7555c453c612c37
masaki344 <masakimiyoshi18.v.v@icloud.com>
2020-02-10 17:01:58 +0900

    ScriptTestプロジェクト　スクリプトの作成
    課題の再提出

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-
+    //配列を宣言して出力しましょう
     void Start ()
     {
         int[] array = { 2, 4, 3, 1, 5 };
-
+        //昇順を出力する
         for (int i = 0; i < array.Length; i++)
         {
             Debug.Log(array[i]);
         }
-
+        //降順を出力する
         for (int i = array.Length - 1; i >= 0; i--)
         {
             Debug.Log(array[i]);
